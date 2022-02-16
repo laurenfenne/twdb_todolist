5798f2d1c687d2bf3d3ac85dcc7681abff4d83e3
Lauren <lauren.ffen@gmail.com>
2022-02-16 16:56:20 +1100

    Added remove task functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let ans = prompt("What would you like to do?");
 const todos = ['Placeholder task', 'Eat'];
 
-//Don't get fooled into the ||or here
+//Don't get fooled into using || here lol
 while (ans !== 'quit' && ans !== 'q') {
     if (ans === 'list' || ans === 'l') {
-        console.log ("Listing TODOS...");
-        /*
+        console.log ("Here are your current tasks:");
+        /* dumb dumb...
         for(let i of todos){
             console.log(`[${}]: ${i}`);
         }*/
@@ -17,6 +17,17 @@ while (ans !== 'quit' && ans !== 'q') {
         let newTask = prompt("Enter your new task");
         todos.push(newTask);
         console.log(`Added task: ${newTask} to position [${todos.length - 1}]`);
+    }
+    else if (ans === 'delete' || ans === 'd') {
+        let killTask = parseInt(prompt("Enter the position of the task you wish to remove"));
+        //make sure an integer has been entered and doesn't exceed bounds of array
+        if(killTask < todos.length && killTask >= 0) {
+            todos.splice(killTask, 1);
+            console.log(`Task [${killTask}] has been removed.`);
+        }
+        else {
+            console.log(`Invalid task position`);
+        }
     }
     ans = prompt("What would you like to do?");
 }
