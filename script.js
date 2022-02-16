b9d4a5f6d7ce7cf123c5afa7701fe2d2323bf82a
Lauren <lauren.ffen@gmail.com>
2022-02-16 16:34:02 +1100

    Added quit and list functionality

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let ans = prompt("What would you like to do?");
+const todos = ['Placeholder task', 'Eat'];
 
-while (ans !== 'quit') {
+//Don't get fooled into the ||or here
+while (ans !== 'quit' && ans !== 'q') {
+    if (ans === 'list' || ans === 'l') {
+        console.log ("Listing TODOS...");
+        /*
+        for(let i of todos){
+            console.log(`[${}]: ${i}`);
+        }*/
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`[${i}]: ${todos[i]}`);
+        }
+    }
     ans = prompt("What would you like to do?");
 }
-console.log ("OK");
+console.log ("OK fine you are a quitter");
